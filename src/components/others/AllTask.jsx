06988fd82bd498e491e5ec89a10dca7a22b4e87c
RITESH0091 <95367480+RITESH0091@users.noreply.github.com>
2025-01-13 04:50:11 +0530

    'ritesh'

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { AuthContext } from '../../context/authProvider'
+import { AuthContext } from '../../context/AuthProvider'
 
 function AllTask() {
 
